d399a96b35d802e70bc0f285ce9961ebaa4e8f40
Christian <Christianmnaira@gmail.com>
2024-07-27 22:46:39 +0800

    feat: Added a content in searchbar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react';
+
+import { Paper, IconButton } from '@mui/material';
+import SearchIcon from "@mui/icons-material/Search";
+import { useNavigate } from 'react-router-dom';
 
 const SearchBar = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+
+    const onhandleSubmit = (e) => {
+
+        alert(searchTerm);
+
+    }
+
   return (
-    <div>
-      SearchBar
-    </div>
+    <Paper
+    component='form'
+    onSubmit={onhandleSubmit}
+    sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: {sm: 5},
+    }}
+    >
+      
+        <input 
+        type="text" 
+        className='search-bar'
+        placeholder='Search...'
+        onChange={(e) => setSearchTerm(e.target.value)}
+        value={searchTerm}
+        />
+
+        <IconButton type="submit" sx={{p: '10px', color: 'red'}}>
+            <SearchIcon/>
+        </IconButton>
+
+    </Paper>
   )
 }
 
