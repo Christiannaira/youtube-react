e3986dd701212c3a3cc9b669a2fb4532542eb703
Christian <Christianmnaira@gmail.com>
2024-07-27 22:19:09 +0800

    feat: Compiled all components in one place to export (index.js)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 
-import Navbar from "./components/Navbar";
-import Feed from "./components/Feed";
-import VideoDetail from "./components/VideoDetail";
-import ChannelDetail from "./components/ChannelDetail";
-import SearchFeed from "./components/SearchFeed";
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from "./components"
 
 function App() {
 
@@ -13,7 +9,7 @@ function App() {
     <>
      <BrowserRouter>
 
-    <Box>
+    <Box sx={{backgroundColor: '#000'}}>
 
       <Navbar/>
 
