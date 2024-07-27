2312ca2174a696e834696295df09fd0a8de7e117
Christian <Christianmnaira@gmail.com>
2024-07-27 22:15:57 +0800

    feat: Added a router path in an app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
+import Navbar from "./components/Navbar";
+import Feed from "./components/Feed";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
 
 function App() {
 
   return (
     <>
-     hello world
+     <BrowserRouter>
+
+    <Box>
+
+      <Navbar/>
+
+      <Routes>
+
+        <Route path="/" exact element={<Feed/>} />
+        <Route path="/video/:id" element={<VideoDetail/>} />
+        <Route path="/channel/:id" element={<ChannelDetail/>} />
+        <Route path="/search/:searchTerm" element={<SearchFeed/>} />
+
+      </Routes>
+
+    </Box>
+
+     </BrowserRouter>
     </>
   )
 }
