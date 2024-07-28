abefd16b76df3ee5ed253742ae0efa4de4dc7675
Christian <Christianmnaira@gmail.com>
2024-07-28 22:06:31 +0800

    feat: Added a videos component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
 const Feed = () => {
   return (
-    <div>
-      Feed
-    </div>
+      <Stack sx={{flexDirection: {sx: 'column', md: 'row'}}}>
+
+          <Box>
+
+            <Typography className='copyright' variant='body2' sx={{mt: 1.5, color: '#fff',}}>
+            Copyright © 2022 JSM Media 
+            </Typography>
+
+          </Box>
+
+          <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+
+            <Typography variant='h4' fontWeight="bold" mb={2} sx={{color: 'white'}}>
+
+            </Typography>
+
+          </Box>
+
+      </Stack>
   )
 }
 
