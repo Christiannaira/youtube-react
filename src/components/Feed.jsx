59b73cf66e2ba4cc10c82ab83898f8ec467522b3
Christian <Christianmnaira@gmail.com>
2024-07-28 22:23:58 +0800

    feat: Added a two icons from material ui in constants js file

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
+import { fetchFromApi } from '../utils/fetchFromApi';
+
+import {Videos, Sidebar} from "./";
+
+
 const Feed = () => {
+
+
   return (
       <Stack sx={{flexDirection: {sx: 'column', md: 'row'}}}>
 
-          <Box>
+          <Box sx={{height: {sx: "auto", md: "92vh"}, borderRight: "1px solid #3d3d3d", px: {sx: 0, md: 2}}}>
+
+          <Sidebar/>
 
             <Typography className='copyright' variant='body2' sx={{mt: 1.5, color: '#fff',}}>
             Copyright © 2022 JSM Media 
@@ -16,7 +25,7 @@ const Feed = () => {
           <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
 
             <Typography variant='h4' fontWeight="bold" mb={2} sx={{color: 'white'}}>
-
+s
             </Typography>
 
           </Box>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
 
+import  MusicNoteIcon  from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
