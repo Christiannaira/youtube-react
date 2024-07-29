e12a000e20105cbf4d2b54c9191780f4d3bdc900
Christian <Christianmnaira@gmail.com>
2024-07-29 22:21:38 +0800

    feat: Edited navbar logo link style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,9 +14,10 @@ const Navbar = () => {
     p={2}
     sx={{position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}
     >
-      <Link to="/" style={{display: 'flex', alignItems: 'center'}}>
+
+    <Link to="/" style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
           <img src={logo} alt="logo" height={45} />
-      </Link>
+    </Link>
     
     <SearchBar/>
 
