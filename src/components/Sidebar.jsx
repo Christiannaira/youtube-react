15135c8d76d6c815a2af9952eee4ef45ff628fda
Christian <Christianmnaira@gmail.com>
2024-07-29 22:50:42 +0800

    feat: Designed the sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,20 +3,48 @@ import React from 'react';
 import { Stack } from '@mui/material';
 
 import {categories} from "../utils/constants";
+import { Category } from '@mui/icons-material';
 
-const Sidebar = () => {
+const Sidebar = ({selectedCategory, setSelectedCategory}) => {
 
   console.log(categories);
 
   return (
-       <Stack>
+       <Stack
+       direction="row"
+       sx={{
+        overflowY: "auto",
+        height: {sx: "auto", md: "95%"},
+        flexDirection: {md: "column"},
+       }}
+       >
 
        {categories.map((category) => (
-        <button>
 
-          {<category.icon/>}
+        <button
+        className='category-btn'
+        onClick={() => {
+         setSelectedCategory(category.name);
+        }}
+        style={{
+          background: category.name === selectedCategory && "#FC1503", color: "white",
+        }}
+        key={category.name}
+        >
+        
+          <span style={{ color: category.name === selectedCategory? "white": "red", marginRight: "15px"}}>
 
-          {category.name}
+              {<category.icon/>}
+
+          </span>
+
+          <span style={{opacity: category.name === selectedCategory? "1": "0.8"}}>
+
+            {category.name}
+
+          </span>
+
+          
 
         </button>
        ))}
