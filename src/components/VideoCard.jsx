b0e489538fd9a825fd154eb2465a1d4cbb2f9ce5
Christian <Christianmnaira@gmail.com>
2024-07-30 09:27:56 +0800

    feat: Displayed the videos thumbnail from youtube api

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom';
 import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-const VideoCard = () => {
+import { demoThumbnailUrl, demoVideoUrl, demoChannelTitle, demoChannelUrl, demoVideoTitle } from '../utils/constants';
+
+const VideoCard = ({video: {id: {videoId}, snippet}}) => {
   return (
-    <div>
-      VideoCard
-    </div>
+      <Card>
+
+        <Link to={'/'}>
+          <CardMedia component="img" image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} sx={{width: {xs: '100%', sm: '358px'}, height: 180}}/>
+        </Link>
+
+      </Card>
   )
 }
 
